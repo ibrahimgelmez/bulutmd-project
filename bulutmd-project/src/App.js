a501440fa132789e5afbfff7c87a68ddf70e6b72
Ibrahim Gelmez <ibrg47@gmail.com>
2023-02-15 20:19:51 +0300

    Header

--- bulutmd-project/src/App.js
+++ bulutmd-project/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <div className="title">
+          <h1 className="header--title">BulutMD</h1>
+          <div className="header--buttons">
+            <button className="login-button">Giriş</button>
+            <button className="trial-button">Deneme Başlat</button>
+          </div>
+          
+        </div>
+        
+        <h2 className="header--downtitle">Popüler Başlıklar</h2>
       </header>
-    </div>
+
+      <main>
+
+      </main>
+
+      <footer>
+        
+      </footer>
+    </>
   );
 }
 
